44bf8fe78887b22dc8eff3c4362d2bdc601cf542
READTalk <redtalk.app@hotmail.com>
2026-07-08 22:59:35 +0700

    Change header text from 'Avatar' to 'Upload'

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Toaster />
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Avatar
+        Upload
       </h1>
       <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
         <Uploader />
